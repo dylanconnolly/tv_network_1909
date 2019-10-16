847dfb22a960874bff6fe8d6d314301246ecfe17
Dylan Connolly <dylanconnolly145@gmail.com>
2019-10-16 09:57:12 -0600

    Add test and method for network highest paid actor/actress

--- lib/network.rb
+++ lib/network.rb
@@ -7,4 +7,17 @@ class Network
     @shows = []
   end
 
+  def add_show(show)
+    @shows << show
+  end
+
+  def highest_paid_actor
+    network_actors = @shows.map do |show|
+      show.characters
+    end
+    character = network_actors.flatten.max_by do |char|
+      char.salary
+    end
+    character.actor
+  end
 end
