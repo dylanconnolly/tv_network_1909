969585faac623c42e3ee777e8209ec20afcc7678
Dylan Connolly <dylanconnolly145@gmail.com>
2019-10-16 10:00:07 -0600

    Add test for network payroll

--- test/network_test.rb
+++ test/network_test.rb
@@ -33,6 +33,15 @@ class NetworkTest < Minitest::Test
   def test_network_can_find_its_highest_paid_actor
     @nbc.add_show(@knight_rider)
     @nbc.add_show(@parks_and_rec)
+
     assert_equal "Amy Poehler", @nbc.highest_paid_actor
+  end
+
+  def test_network_payroll
+    @nbc.add_show(@knight_rider)
+    @nbc.add_show(@parks_and_rec)
+
+    assert_instance_of Hash, @nbc.payroll
+    assert_equal 4, @nbc.payroll.count
   end
 end
